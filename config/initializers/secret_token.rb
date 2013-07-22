f481d2865f87ac93924b73f23e2b8a1cd6a66d80
root <root@tim-dev.mit.edu>
2013-07-22 14:41:46 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Uniiv::Application.config.secret_key_base = 'b75679e572b0aff8145b3b6cb9936b81b5fa81d14c7f52c927dfb495baf4b2ac428264eb9fa254af4854846baa966cf746c7e9ce5c6d5c9539e6fcb79ccfbd76'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Uniiv::Application.config.session_store :cookie_store, key: '_uniiv_session'
