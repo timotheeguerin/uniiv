76321343b87b643e7c134e495f8ceb80a61fed9c
Timothée Guérin <timothee.guerin@outlook.com>
2013-07-26 15:23:53 -0400

    canvas viz

--- app/models/program_group.rb
+++ app/models/program_group.rb
@@ -1,7 +1,9 @@
 class ProgramGroup < ActiveRecord::Base
   belongs_to :parent, :class_name => ProgramGroup
   has_many :subgroups, :class_name => ProgramGroup, :foreign_key => "parent_id"
-  has_and_belongs_to_many  :courses, :class_name => Course
+  has_and_belongs_to_many :courses, :class_name => Course
 
-
+  def type_enum
+    ['all', 'min_nb', 'min_credit']
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,6 @@ module Uniiv
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
+    config.assets.paths << "#{Rails.root}/app/assets/graphs"
   end
 end
