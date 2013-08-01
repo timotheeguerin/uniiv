f780e1b142163abc87c648a6408389c0c555f39f
tlwr <tlornewr@gmail.com>
2013-08-01 16:50:06 -0400

    css

---
+++ app/assets/javascripts/canvassize.js
@@ -0,0 +1,11 @@
+$(window).resize(function()
+{
+  var newheight = $(window).height()-40;
+  $("#fold").css("height",newheight);
+});
+
+$(window).load(function()
+{
+  var newheight = $(window).height()-40;
+  $("#fold").css("height",newheight);
+});
