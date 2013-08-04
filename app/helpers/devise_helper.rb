384b3d1ce6decf7667e5881bbe679b4a7f617aae
tlwr <tlornewr@gmail.com>
2013-08-04 17:39:01 -0400

    styles
    tim pls pull if you overwrite i am mad

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -2,7 +2,7 @@ module DeviseHelper
 def devise_error_messages!
     flash.now[:alert] = ""
     resource.errors.full_messages.each do |error|
-      flash.now[:alert] += "<div style='font-weight:bold;'>" + error + "</div>"
+      flash.now[:alert] += error + "<br>"
     end
     return nil
   end
