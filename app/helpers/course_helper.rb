9a962a1229b04670b935c973bf6f59479e9f7d54
tlwr <tlornewr@gmail.com>
2013-08-09 10:57:53 -0400

    css and helper for course

--- app/helpers/course_helper.rb
+++ app/helpers/course_helper.rb
@@ -1,2 +1,16 @@
 module CourseHelper
+
+  def get_course_css_class(user, course)
+    state = course.get_course_state(user)
+    if state == "completed"
+      return "course_completed"
+    elsif state == "taking"
+      return "course_taking"
+    elsif state == "available"
+      return "course_available"
+    elsif state == "unavailable"
+      return "course_unavailable"
+    end
+  end
+
 end
