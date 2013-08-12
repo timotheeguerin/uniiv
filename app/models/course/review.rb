1956ee13966f79241aec91c9db7e2a3ae9339583
Timothée Guérin <timothee.guerin@outlook.com>
2013-08-12 09:26:49 -0400

    hguku

--- app/models/course/review.rb
+++ app/models/course/review.rb
@@ -1,4 +1,5 @@
 class Course::Review < ActiveRecord::Base
   belongs_to :user, class_name => User
+  belongs_to :course, class_name => Course::Course
   has_many :ratings, class_name => Course::Rating
 end
