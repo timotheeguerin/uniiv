296d8287a8269569284d973e1bf1d8e4deb298d5
Timothée Guérin <timothee.guerin@outlook.com>
2013-08-21 15:50:18 -0400

    signup mark as alpha tester

--- app/models/user_taking_course.rb
+++ app/models/user_taking_course.rb
@@ -1,9 +1,10 @@
 class UserTakingCourse < ActiveRecord::Base
   belongs_to :user, :class_name => User
   belongs_to :course, :class_name => Course::Course
+  belongs_to :semester, :class_name => Course::Semester
 
   def to_s
-    user.to_s + " - " + course.to_s
+    user.to_s + ' - ' + course.to_s + ' ' + semester.to_s
   end
-  
+
 end
