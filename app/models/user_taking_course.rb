7f868df2dc5fbcd004c7772332a649fb81c0ac5e
Timothée Guérin <timothee.guerin@outlook.com>
2013-08-22 15:22:33 -0400

    signup mark as alpha tester

--- app/models/user_taking_course.rb
+++ app/models/user_taking_course.rb
@@ -2,6 +2,7 @@ class UserTakingCourse < ActiveRecord::Base
   belongs_to :user, :class_name => User
   belongs_to :course, :class_name => Course::Course
   belongs_to :semester, :class_name => Course::Semester
+  belongs_to :grade, :class_name => Course::GradingSystemEntity
 
   accepts_nested_attributes_for :semester
 
