b04dcf19c02f49f90ddb6e9fd5af2b53bfb4c683
Timothée Guérin <timothee.guerin@outlook.com>
2013-09-03 11:30:22 -0400

    works

--- app/helpers/course_helper.rb
+++ app/helpers/course_helper.rb
@@ -16,16 +16,20 @@ module CourseHelper
     end
   end
 
-  def get_expr_html(expr)
-    get_node_html(expr.node)
+  def get_expr_html(expr, template = nil)
+    get_node_html(expr.node, template)
   end
 
-  def get_node_html(node)
+  def get_node_html(node, template = nil)
     r = ''
     return '' if node.operation.nil?
     if node.operation == NodeOperation::NODE
       course = node.course
-      r = r + "<a href='#{course_path(:id => course.id)}' data-id='#{course.id}' data-type='course' class='#{get_course_css_class(course)} underline_link'>#{course.to_s}</a>"
+      if template.nil?
+        r = r + "<a href='#{course_path(:id => course.id)}' data-id='#{course.id}' data-type='course' class='#{get_course_css_class(course)} underline_link'>#{course.to_s}</a>"
+      else
+        r += render template, :course => course
+      end
     else
       r = r + ' (' + node.nodes.map! { |k| "#{get_node_html(k)}" }.join(' ' + node.operation + ' ') + ') '
     end
@@ -34,6 +38,11 @@ module CourseHelper
 
   def user_remove_course_form(course)
 
+  end
+
+  def course_list_item(course)
+    invalid_time = (not course.is_time_valid?)
+    render 'course/course_list_item', :course => course, :invalid_time => invalid_time
   end
 
 end
