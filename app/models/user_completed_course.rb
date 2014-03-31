f1e00cf67fad663adf0237b6908743c8b56daaec
Timothee Guerin <timothee.guerin@outlook.com>
2014-03-31 17:55:43 -0400

    Cannot complete a course with a fail grade

--- app/models/user_completed_course.rb
+++ app/models/user_completed_course.rb
@@ -6,12 +6,16 @@ class UserCompletedCourse < ActiveRecord::Base
 
   validates_presence_of :course_id
   validates_presence_of :user_id
-  validates_presence_of :grade_id, :unless=> :advanced_standing
-  validates_presence_of :semester_id, :unless=> :advanced_standing
-  validates_presence_of :year, :unless=> :advanced_standing
+  validates_presence_of :grade_id, :unless => :advanced_standing
+  validates_presence_of :semester_id, :unless => :advanced_standing
+  validates_presence_of :year, :unless => :advanced_standing
   validates_uniqueness_of :course_id, :scope => :user_id
 
   after_validation :remove_course_taking, on: [:create, :update]
+
+  validate do |course|
+    errors.add(:grade, 'Grade cannot be a fail grade') unless course.grade.pass?
+  end
 
   after_save :reindex
   after_destroy :reindex
@@ -29,6 +33,7 @@ class UserCompletedCourse < ActiveRecord::Base
     c_id = course_id
     UserTakingCourse.joins(:course_scenario).where { course_scenario.user.id == user_id && course_id==c_id }.readonly(false).destroy_all
   end
+
   def term
     return nil if year.nil? or semesester.nil?
     Term.new(semester, year)
