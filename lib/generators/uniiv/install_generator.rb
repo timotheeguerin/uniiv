ff3158e97ec60f262179ce237d9471dd34dad885
Timothee Guerin <timothee.guerin@outlook.com>
2014-04-01 15:55:01 -0400

    ADded diff login for main db

--- lib/generators/uniiv/install_generator.rb
+++ lib/generators/uniiv/install_generator.rb
@@ -9,11 +9,17 @@ module Uniiv
 
     argument :_username, type: :string, required: false, desc: 'Database username'
     argument :_password, type: :string, required: false, desc: 'Database password'
+    argument :_username_main, type: :string, required: false, desc: 'Main database username'
+    argument :_password_main, type: :string, required: false, desc: 'Main database password'
 
     def install
       username = ask_for('Database username?', 'root', _username)
       password = ask_for('Database password?', '', _password)
-      template 'local_env_initializer.yml.erb', 'config/local/local_env.yml', :username => username, :password => password
+      username_main = ask_for('Main database username?', 'root', _username)
+      password_main = ask_for('Main database password?', '', _password)
+      template 'local_env_initializer.yml.erb', 'config/local/local_env.yml',
+               :username => username, :password => password,
+               :username_main => username_main, :password_main => password_main
     end
   end
 end
