c23771d04eaad73c8c62ece7d83aa1c0ea553f9f
Timothee Guerin <timothee.guerin@outlook.com>
2014-05-17 13:02:58 +0100

    Fixed some bugs link to adding programversion

--- db/migrate/20140516194303_create_program_program_versions.rb
+++ db/migrate/20140516194303_create_program_program_versions.rb
@@ -2,7 +2,8 @@ class CreateProgramProgramVersions < ActiveRecord::Migration
   def change
     create_table :program_program_versions do |t|
       t.references :program, index: true
-
+      t.integer :start_year
+      t.integer :end_year
       t.timestamps
     end
   end
