0be5e98070de096bcd97590efe229c6f297232bd
Timothee Guerin <timothee.guerin@outlook.com>
2014-05-23 20:00:12 +0200

    More test

--- test/controllers/user_university_controller_test.rb
+++ test/controllers/user_university_controller_test.rb
@@ -2,8 +2,33 @@ require 'test_helper'
 
 class UserUniversityControllerTest < ActionController::TestCase
   include Devise::TestHelpers
-  test 'should get index' do
-    get :index
+
+  test 'should get edit' do
+    user = create(:user)
+    sign_in user
+    @ability.can :edit, user
+    get :edit
     assert_response :success
+    sign_out user
+  end
+
+  test 'should update university' do
+    scenario = create(:course_scenario)
+    user = scenario.user
+    sign_in user
+    @ability.can :edit, user
+
+    program = create(:program_program)
+    scenario.programs << program
+    user.faculty = program.faculty
+    user.save
+    university = create(:university)
+    get :update, :university_id => university.id
+    assert_response :success
+    user.reload
+    assert user.university = university
+    assert user.faculty.nil?
+    assert user.main_course_scenario.programs.size == 0
+    sign_out user
   end
 end
