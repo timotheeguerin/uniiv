99567933f7267e4ebcab66496630706033e272b9
Timothee Guerin <timothee.guerin@outlook.com>
2014-06-22 01:50:50 +0100

    cleanup javascript

--- app/assets/javascripts/user_dashboard.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $(".program_btn").hover(function () {
-        $(this).parent().parent().parent().children().first().children().first().css('text-decoration', 'underline')
-    }, function () {
-        $(this).parent().parent().parent().children().first().children().first().css('text-decoration', 'none')
-    });
-});
