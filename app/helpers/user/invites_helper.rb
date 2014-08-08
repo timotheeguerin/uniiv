5989cce579fa3d79018c404b01e6d3b82246db90
Timothee Guerin <timothee.guerin@outlook.com>
2014-08-08 14:35:41 +0200

    invites manager

--- app/helpers/user/invites_helper.rb
+++ app/helpers/user/invites_helper.rb
@@ -1,5 +1,5 @@
 module User::InvitesHelper
   def user_invite_signup_link(invite)
-    new_user_registration_path(invite_key: invite.key)
+    new_user_registration_url(invite_key: invite.key)
   end
 end
